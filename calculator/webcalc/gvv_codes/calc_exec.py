b745f8c4fadce89612c4785c767da7bb7e42c90e
gadepall <gadepall@gmail.com>
2018-12-13 13:27:30 +0530

    Add existing files

--- calculator/webcalc/gvv_codes/calc_exec.py
+++ calculator/webcalc/gvv_codes/calc_exec.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
 	if request.method == 'GET':
         # show html form
-		return render_template('calc.html')
+		return render_template('calc_bkup.html')
 #	        return '''
 #	            <form method="post">
 #	                <input type="text" name="expression" />
@@ -17,9 +17,18 @@ def index():
 #	        '''
 	elif request.method == 'POST':
         # calculate result
-        	expression = request.form.get('expression')
-        	result = eval(expression)
-        	return 'result: %s' % result
+#		expression = request.form.get('expression')
+#		result = eval(expression)
+#		result = request.form.get('abc', type=int)
+#		if request.form['submit_button'] == "1":
+		if "one" in request.form: 
+			result = '1'
+		if "two" in request.form: 
+			result = '2'
+#		result = request.form.get('abc')
+#		result = eval(abc)
+		return render_template('calc_bkup.html',  result=result)
+#        	return 'result: %s' % result
 
 # run app
 if __name__ == '__main__':
