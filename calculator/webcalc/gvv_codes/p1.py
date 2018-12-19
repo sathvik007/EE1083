8c65ce536b02697c78249362e7378b174754fc9c
gadepall <gadepall@gmail.com>
2018-12-19 19:01:07 +0530

    Add existing files

--- calculator/webcalc/gvv_codes/p1.py
+++
@@ -1,6 +0,0 @@
-#Program using simple concatenation
-
-str1= input("Enter the first string: ");
-str2= input("Enter the second string: ");
-con = str1+str2;
-print("\nString after concatenation: ",con);
